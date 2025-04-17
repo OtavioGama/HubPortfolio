f2ce62e903cc501bf7ecfba1d2d730b226c5d9b5
OtavioGama <oag.gama@gmail.com>
2025-04-17 15:14:03 -0300

    corrigido props da imagem no ProjectCard

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HubPortfolio
+
+
+#DevMedia
+
+#Falar sobre dangerouslySetInnerHTML
+#Organizacao dos projetos e Vite configurado para multiplas entradas e tratar arquivos HTML dentro de /src como recursos
--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,10 +1,10 @@
 import './styles.css';
 
-function ProjectCard({ title, img, description, link }) {
+function ProjectCard({ title, image, description, link }) {
     return (
         <div className='projectCard'>
             <h3>{title}</h3>
-            <img src={img} alt='site' />
+            <img src={image} alt='site' />
             <p>{description}</p>
             <a href={link}>Abrir</a>
         </div>
