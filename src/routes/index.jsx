9b9797aef626b267c4e7a8859d28cad41958c37e
OtavioGama <oag.gama@gmail.com>
2025-04-27 01:05:51 -0300

    Adicionada funçao/componente ScrollToTop

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import ScrollToTop from '../components/ScrollToTop';
+
 import Home from '../pages/Home';
 import DesignDigital from '../projects/DesignDigitalPropsUseState/src/App.jsx';
 
 function MainRoutes() {
     return (
         <BrowserRouter>
+            <ScrollToTop />
+            
             <Routes>
                 <Route exact path='/' element={ <Home/> } />
                 <Route path='/DesignDigital' element={ <DesignDigital/> } />
